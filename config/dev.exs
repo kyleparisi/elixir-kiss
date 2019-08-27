79407032586076a20ad20ebb40aaeb9eb6c370ac
Kyle Parisi <kyle.parisi@robustwealth.com>
2019-08-27 15:46:28 -0400

    Init

---
+++ config/dev.exs
@@ -0,0 +1,10 @@
+config :libcluster,
+  topologies: [
+    gossip_example: [
+      strategy: Elixir.Cluster.Strategy.Gossip,
+      config: [
+        port: 45892,
+        if_addr: "0.0.0.0",
+        multicast_addr: "230.1.1.251",
+        multicast_ttl: 1,
+        secret: "somepassword"]]]
