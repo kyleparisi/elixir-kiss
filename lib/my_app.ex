e910f834c79497fa28f4d6d5aaa361024568c59d
Kyle Parisi <kyleparisi@gmail.com>
2019-09-02 10:16:57 -0400

    ec2 example

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -2,16 +2,13 @@ defmodule MyApp.App do
   use Application
 
   def start(_type, _args) do
+    Logger.configure(level: :info)
     topologies = [
-      gossip_example: [
-        strategy: Elixir.Cluster.Strategy.Gossip,
+      example: [
+        strategy: ClusterEC2.Strategy.Tags,
         config: [
-          port: 45892,
-          if_addr: "0.0.0.0",
-          multicast_addr: "230.1.1.251",
-          multicast_ttl: 1,
-          secret: "somepassword"
-        ]
+          ec2_tagname: "app"
+        ],
       ]
     ]
     children = [
