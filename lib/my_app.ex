0db543e5b070a9e9231bb25498cb867500484359
Kyle Parisi <kyleparisi@gmail.com>
2019-12-02 20:24:45 -0500

    wip

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -12,11 +12,19 @@ defmodule MyApp.App do
 end
 
 defmodule Router do
-  def match("GET", ["/health"]) do
-    "Ok"
+  def match("GET", ["health"], _conn) do
+    "Ok\n"
   end
 
-  def match(_, _) do
+  def match("GET", ["hello", name], _conn) do
+    "Hello #{name}\n"
+  end
+
+  def match("GET", ["hello2", name], _conn) do
+    EEx.eval_file("templates/hello.html.eex", [name: name])
+  end
+
+  def match(_, _, _) do
     {404, "Not Found"}
   end
 end
@@ -30,7 +38,7 @@ defmodule MyPlug do
   end
 
   def call(conn, _opts) do
-    res = Router.match(conn.method, conn.path_info)
+    res = Router.match(conn.method, conn.path_info, conn)
     case res do
      {http_code, body} -> send_resp(conn, http_code, body)
       _ -> send_resp(conn, 200, res)
