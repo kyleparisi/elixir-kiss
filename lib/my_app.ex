75fa1e56e7229cf565b447fa413ca53976a71319
Kyle Parisi <kyleparisi@gmail.com>
2019-12-04 13:32:17 -0500

    wip

--- lib/my_app.ex
+++ lib/my_app.ex
@@ -40,7 +40,7 @@ defmodule Router do
   end
 
   def match("GET", ["hello2", name], _conn) do
-    EEx.eval_file("templates/hello.html.eex", name: name)
+    {:render, "templates/hello.html.eex", %{name: name}}
   end
 
   def match("GET", ["user", _id], conn) do
@@ -66,15 +66,34 @@ defmodule MyPlug do
   def call(conn, _opts) do
     res = Router.match(conn.method, conn.path_info, conn)
 
-    case res do
-      json when is_map(json) ->
-        json_resp(conn, 200, json)
+    case get_req_header(conn, "accept") do
+      # Handle explicit json requests
+      ["application/json"] ->
+        case res do
+          {:render, _template_path, data} ->
+            IO.inspect data
+            json_resp(conn, 200, data)
 
-      {http_code, body} ->
-        send_resp(conn, http_code, body <> "\n")
+          _ ->
+            json_resp(conn, 200, res)
+        end
 
+      # Otherwise, handle the standard signatures
       _ ->
-        send_resp(conn, 200, res <> "\n")
+        case res do
+          json when is_map(json) ->
+            json_resp(conn, 200, json)
+
+          {:render, template_path, data} ->
+            body = EEx.eval_file(template_path, Map.to_list(data))
+            send_resp(conn, 200, "#{body}")
+
+          {http_code, body} ->
+            send_resp(conn, http_code, "#{body}\n")
+
+          _ ->
+            send_resp(conn, 200, "#{res}\n")
+        end
     end
   end
 end
