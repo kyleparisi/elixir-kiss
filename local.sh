51a509de9cbff33c4bbda154ea3c16eb7e782dee
Kyle Parisi <kyleparisi@gmail.com>
2019-12-05 13:11:31 -0500

    wip

--- local.sh
+++ local.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if test -f ".env.default"; then
+  export $(cat .env.default | grep -v ^# | xargs);
+fi
+if test -f ".env"; then
+  export $(cat .env | grep -v ^# | xargs);
+fi
+
 iex -S mix
